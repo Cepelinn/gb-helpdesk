30b12db12bf909cb639be6a8e44ed72f36a5cb27
Leo <leonidvlvi@gmail.com>
2019-11-10 19:35:51 +0300

    [frontend #2] Change registration page layout

--- views/site/registration.php
+++ views/site/registration.php
@@ -7,31 +7,37 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\user\UserRecord */
 
 $this->title = 'Create User';
-$this->params['breadcrumbs'][] = ['label' => 'User Records', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-record-create">
 
-    <h1 class='page-title'><?= Html::encode($this->title) ?></h1>
+    <!-- <h1 class='page-title'><?= Html::encode($this->title) ?></h1> -->
 
-    <div class="user-record-form">
+    <div class="registration">
 
-    	<?php $form = ActiveForm::begin(['id' => 'reg-form']); ?>
+		<?php $form = ActiveForm::begin(['id' => 'reg-form',
+										'class' => 'registration_form',
+										'fieldConfig' => [
+											'template' => '{input}{error}',
+											'options' => ['class' => 'form-group'],
+    									],
+										'errorCssClass' => 'error']);
+		?>
 
-    	<?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+		<?= $form->field($model, 'username')->textInput(['maxlength' => true, 'class' => 'input', 'placeholder' => "Username"])?>
 
-    	<?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+    	<?= $form->field($model, 'password')->passwordInput(['maxlength' => true, 'class' => 'input', 'placeholder' => "Password"]) ?>
 
-    	<?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    	<?= $form->field($model, 'name')->textInput(['maxlength' => true, 'class' => 'input', 'placeholder' => "Name"]) ?>
 
-    	<?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+    	<?= $form->field($model, 'email')->textInput(['maxlength' => true, 'class' => 'input', 'placeholder' => "Email"]) ?>
 
     	<div class="form-group">
-    		<?= Html::submitButton('Save', ['class' => 'btn btn-success btn-block']) ?>
+    		<?= Html::submitButton('Register', ['class' => 'btn btn__approve btn__uppercase']) ?>
     	</div>
 
     	<?php ActiveForm::end(); ?>
 
-    </div>
+	</div>
 
 </div>
