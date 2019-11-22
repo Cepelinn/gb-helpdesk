5df94d109254163925a7d5a56ee34e7876a297c7
Leo <leonidvlvi@gmail.com>
2019-11-22 21:02:00 +0300

    [frontend #5] Admin ticket layout

--- modules/views/admin-ticket/index.php
+++ modules/views/admin-ticket/index.php
@@ -2,34 +2,44 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\Menu;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\tables\TicketSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Tickets';
+$this->title = 'Tickets Administration tools';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<?= Html::a('Общий контроль', ['/admin']);?>
+
 <div class="ticket-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Ticket', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-      <span>!!По запросу "не определен" в фильтре Responsible Name будут отображены заявки без исполнителя!!</span>
-      <br>
-      <br>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<?= Menu::widget([
+    'items' => [
+        ['label' => 'Back to administration tools', 'url' => ['/admin'],'options'=>['class'=>'profile_menu-item']],
+        ['label' => 'Create new ticket', 'url' => ['/admin/admin-ticket/create'],'options'=>['class'=>'profile_menu-item']],
+    ],
+    'options' => [
+                    'id'=>'navid',
+                    'class' => 'navbar-nav',
+                    'style'=>'float: left; font-size: 16px; list-style: none;',
+                    'data'=>'menu',
+    ],
+                'options' => ['class' => 'profile_menu'],
+                'activeCssClass'=>'profile_menu-item__active',
+]); ?>
+    
+    <span>!!По запросу "не определен" в фильтре Responsible Name будут отображены заявки без исполнителя!!</span>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'summary' => false,
+        'tableOptions' => [
+            'class' => 'data-table'
+        ],
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
             'id',
             [
                 'attribute' => 'title',
