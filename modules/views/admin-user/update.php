9675ae6308b06cb2cd43003f23099b75d550e521
Leo <leonidvlvi@gmail.com>
2019-11-24 22:32:04 +0300

    [frontend #5] Fix breadcrumbs and add menu on admin-user update page

--- modules/views/admin-user/update.php
+++ modules/views/admin-user/update.php
@@ -1,20 +1,33 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\Menu;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\user\UserRecord */
 
-$this->title = 'Update User Record: ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'User Records', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = "Update user information";
+$this->params['breadcrumbs'][] = ['label' => 'Administration tools', 'url' => ['/admin'], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['/admin/admin-user'], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = ['label' => "Profile info", 'url' => ['view', 'id' => $model->id], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = $this->title.' ('.$model->username.')';
 ?>
-<?= Html::a('Управление пользователями',
-                        ['/admin/admin-user']);?>
-<div class="user-record-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<?= Menu::widget([
+    'items' => [
+        ['label' => 'Back to profile info', 'url' => ['/admin/admin-user/view', 'id' => $model->id],'options'=>['class'=>'profile_menu-item']],
+        ['label' => 'Change password', 'url' => ['/admin/admin-user/password-change', 'id' => $model->id],'options'=>['class'=>'profile_menu-item']],
+    ],
+    'options' => [
+                    'id'=>'navid',
+                    'class' => 'navbar-nav',
+                    'data'=>'menu',
+                ],
+                'options' => ['class' => 'profile_menu'],
+                'activeCssClass'=>'profile_menu-item__active',
+]); ?>
+
+<div class="user-update">
 
     <?= $this->render('update_form', [
         'model' => $model,
