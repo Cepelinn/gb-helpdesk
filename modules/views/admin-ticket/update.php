fe776e3954272101db4949bff6d8df25792867ed
Leo <leonidvlvi@gmail.com>
2019-11-24 22:31:13 +0300

    [frontend #5] Fix breadcrumbs and add menu on admin-ticket update page

--- modules/views/admin-ticket/update.php
+++ modules/views/admin-ticket/update.php
@@ -1,23 +1,38 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\Menu;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\tables\Ticket */
 
-$this->title = 'Update Ticket: ' . $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Tickets', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = "Update ticket information";
+$this->params['breadcrumbs'][] = ['label' => 'Administration tools', 'url' => ['/admin'], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = ['label' => 'Tickets', 'url' => ['/admin/admin-ticket'], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = ['label' => "Ticket info", 'url' => ['view', 'id' => $model->id], 'class' => 'breadcrumb_link'];
+$this->params['breadcrumbs'][] = $this->title.' ('.$model->title.')';
 ?>
-<div class="ticket-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<?= Menu::widget([
+    'items' => [
+        ['label' => 'Back to ticket info', 'url' => ['/admin/admin-ticket/view', 'id' => $model->id],'options'=>['class'=>'profile_menu-item']],
+    ],
+    'options' => [
+                    'id'=>'navid',
+                    'class' => 'navbar-nav',
+                    'data'=>'menu',
+                ],
+                'options' => ['class' => 'profile_menu'],
+                'activeCssClass'=>'profile_menu-item__active',
+]); ?>
+
+<div class="ticket-update">
     <span>Автор: <?= $model->authorName ?></span>
 
     <?= $this->render('update_form', [
         'model' => $model,
         'statusList' => $statusList,
+        'userList' => $userList,
     ]) ?>
 
 </div>
