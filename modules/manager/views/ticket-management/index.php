b4e501c91d2d77011b64fd098683b8903616406f
Leo <leonidvlvi@gmail.com>
2019-11-29 20:36:33 +0300

    [frontend #8] Fix ticket management index page

--- modules/manager/views/ticket-management/index.php
+++ modules/manager/views/ticket-management/index.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        // 'filterModel' => $searchModel,
         'summary' => false,
         'tableOptions' => [
             'class' => 'data-table'
